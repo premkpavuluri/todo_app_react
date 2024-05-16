00e4a525a164e86e77afb496c693f307285cf29f
Prem Pavuluri <52355836+premkpavuluri@users.noreply.github.com>
2024-05-16 22:17:13 +0530

    Prem | Extracted Todo heading

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import './Todo.css';
 import TodoItems from "./TodoItems";
 import AddTodoItem from "./AddTodoItem";
+import TodoTitle from "./TodoTitle";
 
 const Todo = (props) => {
   const [todoItems, setTodoItems] = useState(
@@ -9,12 +10,12 @@ const Todo = (props) => {
 
   const addTodoItem = (text) => {
     const nextItemId = todoItems.nextItemId + 1;
-    const items = [...todoItems.items, {id: todoItems.nextItemId, text: text}];
+    const items = [...todoItems.items, {id: todoItems.nextItemId, text}];
     setTodoItems({nextItemId, items});
   }
 
   return (<div className="todo-layout">
-    <h1>Todo List</h1>
+    <TodoTitle/>
     <AddTodoItem onClick={addTodoItem}/>
     <TodoItems todoItems={todoItems.items}/>
   </div>);
---
+++ src/components/TodoTitle.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TodoTitle = (props) => {
+  return (<div className="todo-title"><h1>TODO </h1></div>);
+}
+
+export default TodoTitle;
